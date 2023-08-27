26ce38f6e7383d885ad1e81969897c8268f4dc68
luissena <luis.senarode@gmail.com>
2023-08-27 20:11:22 -0300

    fix: cart

--- src/app/(market)/cart/page.tsx
+++ src/app/(market)/cart/page.tsx
@@ -3,24 +3,43 @@ import { useCartStore } from "@/stores/useCartStore"
 import { formatCurrency } from "@/utils/formatCurrency"
 import { Button } from "@nextui-org/react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { MdOutlineArrowBackIosNew } from "react-icons/md"
 
 export default function Cart() {
+  const router = useRouter()
+  const backToPurchase = () => {
+    router.back()
+  }
+  const finishPurchase = () => {
+    alert("Compra finalizada!")
+    console.log(items)
+  }
   const items = useCartStore((state) => state.items)
 
   const total = items.reduce((acc, item) => {
     return acc + item.product.price * item.quantity
   }, 0)
   return (
-    <section className="max-w-5xl mx-5 lg:mx-auto mt-5">
-      <div className="flex flex-col gap-5">
+    <section className="grid grid-cols-2 max-w-5xl mx-5 lg:mx-auto mt-5">
+      <h1 className="text-2xl font-bold col-span-1">Carrinho</h1>
+      <Button
+        onClick={backToPurchase}
+        variant="ghost"
+        className="bg-white  col-span-1 ml-auto flex gap-1 items-center"
+      >
+        <MdOutlineArrowBackIosNew />
+        <span className="text-2xl">Voltar</span>
+      </Button>
+      <div className="flex flex-col gap-5 col-span-2">
         {items.length > 0 ? (
           items.map((item) => (
             <div
-              key={`${item.product}-${item.selectedFlavor}-${item.quantity}`}
+              key={item.product.id}
               className="lg:grid lg:grid-cols-12 gap-3 lg:gap-0 flex flex-col items-center  shadow p-5 rounded-lg"
             >
               <Image
-                src="/images/pexels-narda-yescas-1566837.png"
+                src={`https://naujadmfrtsfswtpbiyx.supabase.co/storage/v1/object/public/products/${item.product.photos[0].url}`}
                 alt="pizza"
                 className="rounded-xl col-span-2 w-1/2 lg:w-auto"
                 width={100}
@@ -30,10 +49,7 @@ export default function Cart() {
                 <span className="text-sm font-light">Produto</span>
                 <span className="font-bold">{item.product.name}</span>
               </div>
-              <div className="col-span-2 flex flex-col items-center lg:items-start">
-                <span className="text-sm font-light">Sabor</span>
-                <span className="font-bold">{item.selectedFlavor}</span>
-              </div>
+
               <div className="col-span-2 flex flex-col items-center lg:items-start">
                 <span className="text-sm font-light">Preço</span>
                 <span className="font-bold">
@@ -57,12 +73,15 @@ export default function Cart() {
         )}
       </div>
 
-      <div className="flex items-center my-5  ">
+      <div className="col-span-2 flex items-center my-5  ">
         <span className="text-xl">
           Total da compra:{" "}
           <span className="font-bold ">{formatCurrency(total)}</span>
         </span>
-        <Button className="ml-auto font-['Bebas_Neue'] text-xl leading-3 tracking-[0.72px] bg-[#FFB521] ">
+        <Button
+          onClick={finishPurchase}
+          className="ml-auto font-['Bebas_Neue'] text-xl leading-3 tracking-[0.72px] bg-[#FFB521] "
+        >
           Finalizar compra
         </Button>
       </div>
--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -5,32 +5,21 @@ import { create } from "zustand"
 export interface Item {
   product: Product
   quantity: number
-  selectedFlavor: Set<string>
 }
 
 interface Store {
   items: Item[]
   addToCart: (item: Item) => void
-  selectedFlavor: Set<string>
-  handleFlavor: (flavor: Set<string>) => void
 }
 
 export const useCartStore = create<Store>((set, get) => {
   return {
     items: [],
-    selectedFlavor: new Set<string>(),
-    handleFlavor: (flavor: Set<string>) => {
-      set({
-        selectedFlavor: new Set(flavor),
-      })
-    },
+
     addToCart: (item: Item) => {
-      localStorage.setItem("cart", JSON.stringify([...get().items, item]))
       set({
         items: [...get().items, item],
       })
-      console.log(get().selectedFlavor.values().next().value)
-      console.log(get().items)
     },
   }
 })
