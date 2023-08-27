0f93a21698051309dbb842a88e64a3ce52550864
luissena <luis.senarode@gmail.com>
2023-08-27 19:01:24 -0300

    fix: redirect default page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: process.env.NEXT_PUBLIC_DEFAULT_REDIRECT,
+        permanent: true,
+      },
+    ]
+  },
   experimental: {
     serverActions: true,
   },
